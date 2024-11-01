2cc730ad232b865f5afd01ed19f8245d6d275495
Grey <grzegorz.swiecinski@gmail.com>
2024-11-01 20:53:45 +0100

    Create a static web / interface

--- Apps/WebAppToDo/web.py
+++ Apps/WebAppToDo/web.py
@@ -12,8 +12,18 @@ st.title("My ToDo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase you productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        print(checkbox)
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Enter a todo: ", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
+
+st.session_state
+
+print("Helo")
